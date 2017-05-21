5288018b5602e7268e54ecf84892b36e95419bc6
chanshunli <chanshunli@gmail.com>
2017-05-21 08:51:58 +0800

    Update project.clj & add test-examples.clj

--- project.clj
+++ project.clj
@@ -7,5 +7,9 @@
                  [hcadatalab/powderkeg "0.5.1"]
                  [com.esotericsoftware/kryo-shaded "4.0.0"]  ;; For Spark 2.x support
                  [org.apache.spark/spark-core_2.11 "2.1.0"]
-                 [org.apache.spark/spark-streaming_2.11 "2.1.0"]]
+                 [org.apache.spark/spark-sql_2.11 "2.1.0"]
+                 [org.apache.spark/spark-streaming_2.11 "2.1.0"]
+                 [org.apache.spark/spark-mllib_2.11 "2.1.0"]
+                 [clj-time "0.12.2"]]
+  :aot :all
   :repositories {"aliyun" "http://maven.aliyun.com/nexus/content/groups/public"})
